ebf823e44a55eaf8df6e61275ac4fe4d8d4b7687
Gold <gold@Golds-Mac.local>
2020-11-16 00:04:03 -0800

    fix

--- src/screens/singleChatListScreen.js
+++ src/screens/singleChatListScreen.js
@@ -86,13 +86,13 @@ export default class SingleChatListScreen extends React.Component {
     var now = new Date();
     let secondsPast = (now.getTime() - timeStamp) / 1000;
     if (secondsPast < 60) {
-      return parseInt(secondsPast) + 's';
+      return parseInt(secondsPast) + 's ago';
     }
     if (secondsPast < 3600) {
-      return parseInt(secondsPast / 60) + 'm';
+      return parseInt(secondsPast / 60) + 'm ago';
     }
     if (secondsPast <= 86400) {
-      return parseInt(secondsPast / 3600) + 'h';
+      return parseInt(secondsPast / 3600) + 'h ago';
     }
     if (secondsPast > 86400) {
       let day = d.getDate();
